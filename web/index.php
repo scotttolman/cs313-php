6070ec0be5e07b94ff63feb2915c25e62b016e53
scotttolman <scotttolman@gmail.com>
2018-01-19 17:06:55 -0600

    Starting personal page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Scott Tolman</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="tolman.css" />
+  <script src="tolman.js"></script>
+</head>
+<body>
+  <div>Scott's Page</div>
+</body>
+</html>
