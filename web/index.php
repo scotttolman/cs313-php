bd1facedb9c176d1163ee50cd9289bed0c7ff4eb
scotttolman <scotttolman@gmail.com>
2018-01-20 14:00:12 -0600

    added <code> tag

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@
     <title>Scott Tolman</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="tolman.css" />
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+    <link rel="icon" href="favicon.ico" />
     <script src="tolman.js"></script>
 </head>
 
@@ -17,7 +17,7 @@
         <h1>About Me</h1>
         <p id="info">My name is Scott Tolman. I am a Software Engineering major. I do school full-time and don't currently work outside
                 the house. I have 6 kids ages 7, 7, 5, 3, 0, and 0. I don't really know what I am interested in because I haven't
-                had time to figure that out with all these kids. I do enjoy programming though.</p>
+                had time to figure that out with all these kids. <code>I do enjoy programming though.</code></p>
         <img src="family.JPG" alt="Family Photo" id="picture" onmouseover="changePic()" onmouseout="changeBack()">
     </div>
 </body>
