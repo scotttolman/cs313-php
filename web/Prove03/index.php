fcda4bf6c6cd93b615852b67acfabc92cc7b0dab
scotttolman <scotttolman@gmail.com>
2018-01-27 15:42:07 -0600

    added forms to submit to cart

--- web/Prove03/index.php
+++ web/Prove03/index.php
@@ -35,7 +35,7 @@ session_start(); //must be first thing on page
         <div id="balls" onmouseover="showBalls()">
             Poké Balls <img src="Pokeball.png" alt="Pokeball" id="pbpic">
         </div>
-        <div id="cart">
+        <div id="cart" onClick="document.forms['hiddenCart'].submit();">
             Cart <img src="backpack.png" alt="backpack" id="backpackpic">
         </div>
     </div>
@@ -44,32 +44,32 @@ session_start(); //must be first thing on page
             <h3>Mega Drain</h3>
             <p>Mega Drain deals damage and the user will recover 50% of the HP drained</p>
             <img src="MD.png" alt="Mega Drain">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="MD" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('MD')">Add to cart</button>
             <h4>¥ 2000</h4>
         </div>
         <div id="TM2">
             <h3>Fire Blast</h3>
             <p>Fire Blast deals damage and has a 10% chance of burning the target</p>
             <img src="FB.png" alt="Fire Blast">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="FB" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('FB')">Add to cart</button>
             <h4>¥ 2000</h4>
         </div>
         <div id="TM3">
             <h3>Water Gun</h3>
             <p>Water Gun deals damage with no additional effect</p>
             <img src="WG.png" alt="Water Gun">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="WG" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('WG')">Add to cart</button>
             <h4>¥ 2000</h4>
         </div>
         <div id="TM4">
             <h3>Thunderbolt</h3>
             <p>Thunderbolt deals damage and has a 10% chance of paralyzing the target</p>
             <img src="TB.png" alt="Thunderbolt">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="TB" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('TB')">Add to cart</button>
             <h4>¥ 2000</h4>
         </div>
     </div>
@@ -78,16 +78,16 @@ session_start(); //must be first thing on page
             <h3>Poké Ball</h3>
             <p>A device for ballching wild Pokémon. It is thrown like a ball at the target. It is designed as a capsule system</p>
             <img src="pokeball.png" alt="Poké Ball">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="PB" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('PB')">Add to cart</button>
             <h4>¥ 200</h4>
         </div>
         <div id="ball2">
             <h3>Great Ball</h3>
             <p>A good, high-performance Ball that provides a higher Pokémon catch rate than a standard Poké Ball</p>
             <img src="great.png" alt="Great Ball">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="GB" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('GB')">Add to cart</button>
             <h4>¥ 1,000</h4>
         </div>
         <div id="ball3">
@@ -95,18 +95,28 @@ session_start(); //must be first thing on page
             <p>An ultra-performance Ball that provides a higher Pokémon catch rate than a Great Ball</p>
             <br>
             <img src="ultra.png" alt="Ultra Ball">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="UB" onkeyup="this.value = MinMax(this.value, 0, 99)" onclick="this.select()">
+            <button onclick="addItem('UB')">Add to cart</button>
             <h4>¥ 5,000</h4>
         </div>
         <div id="ball4">
             <h3>Master Ball</h3>
             <p>The best Ball with the ultimate level of performance. It will catch any wild Pokémon without fail</p>
             <img src="master.png" alt="Master Ball">
-            <input type="text" value="0" onkeyup="this.value = MinMax(this.value, 0, 1)" onclick="this.select()">
-            <button>Add to cart</button>
+            <input type="text" value="0" name="MB" onkeyup="this.value = MinMax(this.value, 0, 1)" onclick="this.select()">
+            <button onclick="addItem('MB')">Add to cart</button>
             <h4>¥ 20,000</h4>
         </div>
+        <form action="cart.php" method="POST" id="hiddenCart">
+            <input type="hidden" id="_MD">
+            <input type="hidden" id="_FB">
+            <input type="hidden" id="_WG">
+            <input type="hidden" id="_TB">
+            <input type="hidden" id="_PB">
+            <input type="hidden" id="_GB">
+            <input type="hidden" id="_UB">
+            <input type="hidden" id="_MB">
+        </form>
     </div>
 </body>
 </html>
--- web/Prove03/pokestore.js
+++ web/Prove03/pokestore.js
@@ -1,3 +1,9 @@
+// TMs
+var MD = 0, FB = 0, WG = 0, TB = 0;
+// Balls
+var PB = 0, GB = 0, UB = 0, MB = 0;
+
+
 function colorChange() {
     switch(document.getElementById("banner").style.backgroundColor){
         case "yellow":
@@ -36,5 +42,35 @@ function MinMax(val, min, max) {
     }
     else {
         return val;
+    }
+}
+
+function addItem(id) {
+    var qt = document.getElementsByName(id).value;
+    switch (id) {
+        case "MD":
+            MD = qt;
+            break;
+        case "FB":
+            FB = qt;
+            break;
+        case "WG":
+            WG = qt;
+            break;
+        case "TB":
+            TB = qt;
+            break;
+        case "PB":
+            PB = qt;
+            break;
+        case "GB":
+            GB = qt;
+            break;
+        case "UB":
+            UB = qt;
+            break;
+        case "MB":
+            MB = qt;
+            break;
     }
 }
