151b909221ba78860aa87aa13ca81bebc9334a3c
scotttolman <scotttolman@gmail.com>
2018-02-23 12:01:39 -0600

    changed db connection for heroku

--- web/cit261/json_ajax_local_dom/ajax.php
+++ web/cit261/json_ajax_local_dom/ajax.php
@@ -3,17 +3,28 @@
 $jString = $_POST['x'];
 
 try
-  {
-    $dbuser = 'postgres';
-    $dbpassword = 'CS313-PHP';
-    $db = new PDO('pgsql:host=127.0.0.1;dbname=ajax', $dbuser, $dbpassword);
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+{
+//   $dbuser = 'postgres';
+//   $dbpassword = 'CS313-PHP';
+//   $db = new PDO('pgsql:host=127.0.0.1;dbname=ajax', $dbuser, $dbpassword);
+
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 
 $sql = "UPDATE jsontext SET jtext = (:txt)";
 $stmt = $db->prepare($sql);
--- web/cit261/json_ajax_local_dom/ajaxGet.php
+++ web/cit261/json_ajax_local_dom/ajaxGet.php
@@ -1,17 +1,28 @@
 <?php
 
 try
-  {
-    $dbuser = 'postgres';
-    $dbpassword = 'CS313-PHP';
-    $db = new PDO('pgsql:host=127.0.0.1;dbname=ajax', $dbuser, $dbpassword);
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+{
+//   $dbuser = 'postgres';
+//   $dbpassword = 'CS313-PHP';
+//   $db = new PDO('pgsql:host=127.0.0.1;dbname=ajax', $dbuser, $dbpassword);
+
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 
 $sql = 'SELECT jtext FROM jsontext';
 $stmt = $db->prepare($sql);
